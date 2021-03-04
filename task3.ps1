35329beac48c632abe0d49cb77d5e0b368cb3e7f
michaelkotor <milan3020@gmail.com>
2021-03-04 04:50:33 +0200

    thid task

---
+++ task3.ps1
@@ -0,0 +1,9 @@
+$logs = Get-EventLog -LogName System -Newest 10
+
+Function GetLogsOfApplication($name) {
+    $array = Get-EventLog -LogName Application | Where-Object {$_.Source -eq $name}
+    $array > "$name.txt"
+    "Total: " + $array.Count >>  "$name.txt"
+}
+
+GetLogsOfApplication "EventSystem"
